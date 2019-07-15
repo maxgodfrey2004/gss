e812f848d1c907db9efced35176cb47c9efd4549
Max Godfrey <mwugodfrey@gmail.com>
2019-07-15 19:26:49 +0800

    Fix indent

--- benchmarks/run_benchmarks.py
+++ benchmarks/run_benchmarks.py
@@ -21,29 +21,31 @@ import benchmark
 GENFILE_NAME = 'generate_out.txt'
 
 def test_small():
-  """Benchmarks grep and gss on smaller files.
-  """
-  benchmark.benchmark_all(GENFILE_NAME, 5, 5, [1, 2, 3, 4, 5])
-  benchmark.benchmark_all(GENFILE_NAME, 10, 10, [2, 4, 6, 8, 10])
+    """Benchmarks grep and gss on smaller files.
+    """
+    benchmark.benchmark_all(GENFILE_NAME, 5, 5, [1, 2, 3, 4, 5])
+    benchmark.benchmark_all(GENFILE_NAME, 10, 10, [2, 4, 6, 8, 10])
 
 def test_medium():
-  """Benchmarks grep and gss on average sized files.
-  """
-  benchmark.benchmark_all(GENFILE_NAME, 100, 100, [10, 30, 50, 70, 99])
-  benchmark.benchmark_all(GENFILE_NAME, 1000, 1000, [111, 333, 555, 777, 999])
+    """Benchmarks grep and gss on average sized files.
+    """
+    benchmark.benchmark_all(GENFILE_NAME, 100, 100,
+                            [10, 30, 50, 70, 99])
+    benchmark.benchmark_all(GENFILE_NAME, 1000, 1000,
+                            [111, 333, 555, 777, 999])
 
 def test_large():
-  """Benchmarks grep and gss on relatively large files.
-  """
-  benchmark.benchmark_all(GENFILE_NAME, 100000, 1000,
-                          [1, 3, 5, 7, 12, 20, 35, 50, 111, 555, 777, 10000])
+    """Benchmarks grep and gss on relatively large files.
+    """
+    benchmark.benchmark_all(GENFILE_NAME, 100000, 1000,
+                            [1, 3, 5, 7, 12, 20, 35, 50, 111, 555, 777, 10000])
 
 def test_all():
-  """Runs all testing utilities.
-  """
-  test_small()
-  test_medium()
-  test_large()
+    """Runs all testing utilities.
+    """
+    test_small()
+    test_medium()
+    test_large()
 
 if __name__ == '__main__':
-  test_all()
+    test_all()
